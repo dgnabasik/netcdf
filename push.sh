bcb89838147fb5c77031eed9a2dde5ea757512aa
dgnabasik <David.Gnabasik@iShift.io>
2023-05-08 18:38:04 +0200

    Release 1.0.1

--- push.sh
+++ push.sh
@@ -1,5 +1,8 @@
-echo dgnabasik
-git pull https://github.com/dgnabasik/netcdf
+echo dgnabasik in netcdf
+eval "$(ssh-agent -s)"
+git remote -v
+ssh -T -ai /home/david/.ssh/netcdf_ed25519 git@github.com
+git pull git@github.com:dgnabasik/netcdf.git
 echo -n "push?"
 read
 git add --all :/
